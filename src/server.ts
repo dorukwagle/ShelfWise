8531c4745a0ed532d9cac4973e2d1b7e55acca45
dorukwagle <dorukwagle@gmail.com>
2024-07-11 22:52:30 +0545

    handle and log errors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,33 @@
 import app from "./app";
-
+import logger from "./startup/logging";
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+
+process.on('uncaughtException', (err) => {
+  logger.error('Uncaught Exception', { message: err.message, stack: err.stack });
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, p) => {
+  logger.error('Unhandled Rejection', { reason, p });
+  process.exit(1);
+});
+
+const gracefulShutdown = () => {
+  server.close(() => {
+    console.log('Process terminated');
+  });
+  setTimeout(() => {
+    console.log('Forcefully terminated');
+    process.exit(1)
+  }, 5000); // Force exit if not closed within 5 seconds
+};
+
+process.on('SIGTERM', gracefulShutdown);
+process.on('SIGINT', gracefulShutdown);
+
+
+const server = app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
 });
+
