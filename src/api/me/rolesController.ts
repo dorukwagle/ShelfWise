6f01dddd4d7bd00aecae96f314025fbe196d5d1d
dorukwagle <dorukwagle@gmail.com>
2024-07-20 13:58:59 +0545

    upgrade roles route include detailed or short roles info

--- src/api/me/rolesController.ts
+++ src/api/me/rolesController.ts
@@ -3,8 +3,10 @@ import prismaClient from "../../utils/prismaClient";
 
 const roles = express.Router();
 
-roles.get("/", async (req, res) => {
+roles.get("/:detailed?", async (req, res) => {
     const data = await prismaClient().userRoles.findMany();
+    if (req.params.detailed) return res.json(data);
+
     const roles:{[key: string]: number} = {};
     for (const role of data)
         roles[role.role] = role.precedence;
