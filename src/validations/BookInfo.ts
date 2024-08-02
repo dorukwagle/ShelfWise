1a78c3ad1af09b101688bd52bee222414221b55f
dorukwagle <dorukwagle@gmail.com>
2024-08-02 08:42:27 +0545

    refine book info validations

--- src/validations/BookInfo.ts
+++ src/validations/BookInfo.ts
@@ -1,10 +1,78 @@
 import {z} from "zod";
+import {exists, unique} from "../utils/dbValidation";
 
 // unique array values shouldn't be duplicate in requests too
+const uniqueClassNumber = async (cn: string) => unique("bookInfo", "classNumber", cn);
+
+const uniqueBookNumber = async  (bookNumber: string) => unique("bookInfo", "bookNumber", bookNumber);
+
+const publisherExists = async (publisherId: string) => exists("publishers", "publisherId", publisherId);
+
+const eachBookAuthorsExists = async (authorIds: string[]) => {
+    const map = new Map();
+
+    for (const authorId of authorIds) {
+        if (map.has(authorId)) return false;
+        map.set(authorId, 1);
+
+        let exist = await exists("bookInfo", "authorId", authorId);
+        if (!exist)
+            return false;
+    }
+
+    return true;
+}
+
+const eachUniqueIsbn = async (isbns: string[]) => {
+    const map = new Map();
+
+    for (const isbn of isbns) {
+        if (map.has(isbn)) return false;
+        map.set(isbn, 1);
+
+        let exist = await exists("isbns", "isbn", isbn);
+        if (exist)
+            return false;
+    }
+
+    return true;
+}
+
+const eachBookGenreExists = async (genreIds: string[]) => {
+    const map = new Map();
+
+    for (const genreId of genreIds) {
+        if (map.has(genreId)) return false;
+        map.set(genreId, 1);
+
+        let exist = await exists("genres", "genreId", genreId);
+        if (!exist)
+            return false;
+    }
+
+    return true;
+}
+
+const eachUniqueBarcode = async (barcodes: string[]) => {
+    const map = new Map();
+
+    for (const barcode of barcodes) {
+        if (map.has(barcode)) return false;
+        map.set(barcode, 1);
+
+        let exist = await exists("books", "barcode", barcode);
+        if (exist)
+            return false;
+    }
+
+    return true;
+}
 
 const BookInfo = z.object({
-    classNumber: z.string({required_error: "class number is required"}), //unique
-    bookNumber: z.string({required_error: "book number is required"}), //unique
+    classNumber: z.string({required_error: "class number is required"})
+        .refine(uniqueClassNumber, "each class number must be unique"),
+    bookNumber: z.string({required_error: "book number is required"})
+        .refine(uniqueBookNumber, "each book number must be unique"),
     title: z.string({required_error: "title is required"}),
     subTitle: z.string().optional(),
     editionStatement: z.string().optional(),
@@ -14,13 +82,18 @@ const BookInfo = z.object({
     seriesStatement: z.string().optional(),
     addedDate: z.coerce.date().optional(),
     coverPhoto: z.string({required_error: "cover photo is required"}),
-    publisherId: z.string({required_error: "publisher is required"}), //exists
-    bookAuthors: z.string().array(), // each exists
-    isbns: z.string().array(), // unique
-    bookGenres: z.string().array(), //each exists
+    publisherId: z.string({required_error: "publisher is required"})
+        .refine(publisherExists, "publisher not found"),
+    bookAuthors: z.string().array()
+        .refine(eachBookAuthorsExists, "each book author must exist"),
+    isbns: z.string().array()
+        .refine(eachUniqueIsbn, "each isbn must be unique"),
+    bookGenres: z.string().array()
+        .refine(eachBookGenreExists, "each book genre must exist"),
     pricePerPiece: z.coerce.number().min(0),
     totalPieces: z.coerce.number().min(1),
-    barcodes: z.string().array(), // each unique, length equal to totalPieces
+    barcodes: z.string().array() // length equal to totalPieces
+        .refine(eachUniqueBarcode, "each barcode must be unique"),
 });
 
 export type BookInfoType = z.infer<typeof BookInfo>;
